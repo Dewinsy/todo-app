9569b4db0c9e36f00916a58556b6b0f19dc06849
Dewinsy <mushdaisy71@gmail.com>
2021-05-10 16:07:12 +0300

    hosting React App on github pages

--- src/index.js
+++ src/index.js
@@ -2,8 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+//To make sure the app is hoted on github pages without returning a 404 error!
+import {HashRouter as Router} from 'react-router-dom';
+
 ReactDOM.render(
-  <App />, document.getElementById('root')
+  <Router>
+    <App />
+  </Router>
+  , document.getElementById('root')
 );
 
 
